0e866b54c8c0aec72f5b00ecfe6ec45ff06cc895
Myuuer <71081427+Myuuer@users.noreply.github.com>
2025-02-14 19:47:20 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+import requests
+import base64
+import json
+import pyaes
+import binascii
+from datetime import datetime
+import yaml  # 新增依赖
+
+print("      H͜͡E͜͡L͜͡L͜͡O͜͡ ͜͡W͜͡O͜͡R͜͡L͜͡D͜͡ ͜͡E͜͡X͜͡T͜͡R͜͡A͜͡C͜͡T͜͡ ͜͡S͜͡S͜͡ ͜͡N͜͡O͜͡D͜͡E͜͡")
+print("𓆝 𓆟 𓆞 𓆟 𓆝 𓆟 𓆞 𓆟 𓆝 𓆟 𓆞 𓆟")
+print("Author : 𝐼𝑢")
+print(f"Date   : {datetime.today().strftime('%Y-%m-%d')}")
+print("Version: 1.0")
+print("𓆝 𓆟 𓆞 𓆟 𓆝 𓆟 𓆞 𓆟 𓆝 𓆟 𓆞 𓆟")
+print("𝐼𝑢:")
+print(r"""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠤⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠁⠀⡰⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡎⢀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀
+⣀⠀⠀⠀⠀⠀⠀⡠⠬⡡⠬⡋⠀⡄⠀⠀⠀⠀⠀⠀⠀
+⡀⠁⠢⡀⠀⠀⢰⠠⢷⠰⠆⡅⠀⡇⠀⠀⠀⣀⠔⠂⡂
+⠱⡀⠀⠈⠒⢄⡸⡑⠊⢒⣂⣦⠄⢃⢀⠔⠈⠀⠀⡰⠁
+⠀⠱⡀⠀⠀⡰⣁⣼⡿⡿⢿⠃⠠⠚⠁⠀⠀⢀⠜⠀⠀
+⠀⠀⠐⢄⠜⠀⠈⠓⠒⠈⠁⠀⠀⠀⠀⠀⡰⠃⠀⠀⠀
+⠀⠀⢀⠊⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠾⡀⠀⠀⠀⠀
+⠀⠀⢸⣄⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡇⠀⠀⠀⠀
+⠀⠀⠸⢸⣳⠦⣍⣁⣀⣀⣀⣀⣠⠴⠚⠁⠇⠀⠀⠀⠀
+⠀⠀⠀⢳⣿⠄⠸⠢⠍⠉⠉⠀⠀⡠⢒⠎⠀⠀⠀⠀⠀
+⠀⠀⠀⠣⣀⠁⠒⡤⠤⢤⠀⠀⠐⠙⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠣⡀⡼⠀⠀⠈⠱⡒⠂⡸⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢒⠁⠀⠀⠀⠀⠀⠀⠀
+""")
+
+a = 'http://api.skrapp.net/api/serverlist'
+b = {
+    'accept': '/',
+    'accept-language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
+    'appversion': '1.3.1',
+    'user-agent': 'SkrKK/1.3.1 (iPhone; iOS 13.5; Scale/2.00)',
+    'content-type': 'application/x-www-form-urlencoded',
+    'Cookie': 'PHPSESSID=fnffo1ivhvt0ouo6ebqn86a0d4'
+}
+c = {'data': '4265a9c353cd8624fd2bc7b5d75d2f18b1b5e66ccd37e2dfa628bcb8f73db2f14ba98bc6a1d8d0d1c7ff1ef0823b11264d0addaba2bd6a30bdefe06f4ba994ed'}
+d = b'65151f8d966bf596'
+e = b'88ca0f0ea1ecf975'
+
+def decrypt_data(g, d, e):
+    h = pyaes.AESModeOfOperationCBC(d, iv=e)
+    decrypted = b''.join(h.decrypt(g[j:j+16]) for j in range(0, len(g), 16))
+    return decrypted[:-decrypted[-1]]
+
+j = requests.post(a, headers=b, data=c)
+
+if j.status_code == 200:
+    encrypted_data = j.text.strip()
+    binary_data = binascii.unhexlify(encrypted_data)
+    decrypted_data = decrypt_data(binary_data, d, e)
+    server_list = json.loads(decrypted_data)
+    
+    # 构建Clash配置
+    clash_config = {
+        "proxies": [],
+        "proxy-groups": [
+            {
+                "name": "Auto",
+                "type": "url-test",
+                "proxies": [],
+                "url": "http://www.gstatic.com/generate_204",
+                "interval": 300
+            }
+        ],
+        "rules": ["MATCH,Auto"]
+    }
+    
+    for server in server_list['data']:
+        # 添加代理信息
+        clash_config["proxies"].append({
+            "name": server['title'],
+            "type": "ss",
+            "server": server['ip'],
+            "port": server['port'],
+            "cipher": "aes-256-cfb",
+            "password": server['password'],
+            "udp": True  # 可选参数
+        })
+        # 将代理添加到组
+        clash_config["proxy-groups"][0]["proxies"].append(server['title'])
+    
+    # 生成YAML文件
+    with open("clash.yaml", "w", encoding="utf-8") as f:
+        yaml.dump(clash_config, f, allow_unicode=True, sort_keys=False)
+    
+    print("Clash配置文件已生成：clash.yaml")
+else:
+    print(f"请求失败，状态码：{j.status_code}")
